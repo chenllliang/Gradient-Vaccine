28633c0d01ad4cc0b1bb4dc01a3d8bd4419a1f46
Chen Liang <leo.liang.chen@outlook.com>
2022-08-01 01:23:29 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,6 +3,9 @@ pip install portalocker==1.0.0
 pip install tensorboardX
 pip install jupyter
 pip install tensorboard
+
+# for windows user, should be in adminitrator mode and install visual studio .
+
 cd fairseq
 pip install --editable ./
 pip install sacremoses
